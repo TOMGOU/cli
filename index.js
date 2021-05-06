dafc547a80e83ed72c546f79eab1353cc588d1f2
tommy.tang <tommy.tang@lixinchuxing.com>
2021-05-06 16:37:08 +0800

    opti: 添加多选

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const { program } = require('commander')
 const inquirer = require('inquirer')
 const path = require("path")
 const childProcess = require("child_process")
+const fuzzy = require('fuzzy');
+
+inquirer.registerPrompt('checkbox-plus', require('inquirer-checkbox-plus-prompt'));
+
+const configs = ['babel', 'typescript', 'eslint', 'router'];
 
 program
   .arguments('<dir>')
@@ -19,6 +24,31 @@ program
           'react',
           'uni-app'
         ]
+      },
+      {
+        type: 'checkbox-plus',
+        name: 'configs',
+        message: 'Enter configs',
+        pageSize: 10,
+        highlight: true,
+        searchable: true,
+        default: ['babel', 'router'],
+        source: function(answersSoFar, input) {
+          input = input || '';
+    
+          return new Promise(function(resolve) {
+    
+            var fuzzyResult = fuzzy.filter(input, configs);
+    
+            var data = fuzzyResult.map(function(element) {
+              return element.original;
+            });
+    
+            resolve(data);
+            
+          });
+    
+        }
       }
     ]).then(answer => {
       const fullDir = path.resolve(process.cwd(), dir)
